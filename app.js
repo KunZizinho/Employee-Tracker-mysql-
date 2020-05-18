fd269a84e5967fece9c953837471acfaeb5885d4
KunZizinho <kun.zizinho@gmail.com>
2020-05-18 07:28:40 -0400

    adding command line blocks for adding,viewing,updating and deleting

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var inquirer = requirre("inquirer");
+var inquirer = require("inquirer");
 
 
 var connection = mysql.createConnection({
@@ -23,27 +23,156 @@ var connection = mysql.createConnection({
 
   function startApp(){
     inquirer.prompt({
-        name:"department",
+        name:"action",
         type:"list",
         message:"Welcome to Employee-tracker app",
         choices:["Add", "View", "Update", "Delete", "Exit"]
     }).then(function(res){
-        switch (res) {
+        switch (res.action) {
             case "Add":
                 initAdd();
+                break;
 
             case "View":
                 initView();
+                break;
 
             case "Update":
                 initUpdate();
+                break;
 
             case "Delete":
                 initDelete();
+                break;
 
             case "Exit":
                 break;
 
         }
     })
+  
+
+  function initAdd(){
+      inquirer.prompt({
+          name:"add",
+          type:"list",
+          message:"What would you like to add?",
+          choices:["Add Employee", "Add Department", "Add Role", "Go Back", "Exit"]
+      }).then(function(res){
+        switch (res.add) {
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "Add Department":
+                addDepartment();
+                break;
+
+            case "Add Role":
+                addRole();
+                break;
+
+            case "Go Back":
+                startApp();
+                break;
+
+            case "Exit":
+                break;
+
+        }
+      })
   }
+
+  function initView(){
+    inquirer.prompt({
+        name:"view",
+        type:"list",
+        message:"What would you like to add?",
+        choices:["View Employee", "View Department", "View Role","Go Back", "Exit"]
+    }).then(function(res){
+      switch (res.view) {
+          case "View Employee":
+              viewEmployee();
+              break;
+
+          case "View Department":
+              viewDepartment();
+              break;
+
+          case "View Role":
+              viewRole();
+              break;
+
+          case "Go Back":
+              startApp();
+              break;
+
+          case "Exit":
+              break;
+
+      }
+    })
+}
+
+function initUpdate(){
+    inquirer.prompt({
+        name:"update",
+        type:"list",
+        message:"What would you like to add?",
+        choices:["Update Employee", "Update Managment", "Update Role","Go Back", "Exit"]
+    }).then(function(res){
+      switch (res.update) {
+          case "Update Employee":
+              updateEmployee();
+              break;
+
+          case "Update Managment":
+              updateManagment();
+              break;
+
+          case "Update Role":
+              updateRole();
+              break;
+
+          case "Go Back":
+               startApp();
+               break;
+
+          case "Exit":
+              break;
+
+      }
+    })
+}
+
+function initDelete(){
+    inquirer.prompt({
+        name:"delete",
+        type:"list",
+        message:"What would you like to add?",
+        choices:["Delete Employee", "Delete Managment", "Delete Role","Go Back", "Exit"]
+    }).then(function(res){
+      switch (res.delete) {
+          case "Delete Employee":
+              deleteEmployee();
+
+          case "Delete Managment":
+              deleteManagment();
+
+          case "Delete Role":
+              deleteRole();
+
+          case "Go Back":
+              startApp();
+              break;
+
+          case "Exit":
+              break;
+
+      }
+    })
+}
+
+
+
+}
